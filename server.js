045ff6bb60d70e7da319c825252d9d01d9464782
maximren <renoa325@gmail.com>
2020-01-14 16:08:40 +0200

    Fix crc16

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const crc = require('crc');
+// const crc = require('crc');
 
 
 const port = new SerialPort("/dev/tty.usbserial-DABIOSAN", {
@@ -16,9 +16,15 @@ port.pipe(parser);
 SerialPort.list().then(port => console.log(port))
 
 parser.on('data', data => console.log(data));
-port.write("kdkdk", null, (err) => console.log('err', err));
 
-const buffer = new Buffer.alloc(11, [11, 4,  1, 0x10, 0]);
+const buffer = new Buffer(11);
+buffer[0] = 11;
+buffer[1] = 4;
+buffer[2] = 1;
+buffer[3] = 0x10;
+buffer[4] = 0;
+
+const byteBuffer = new Uint8Array(buffer)
 
 port.on("open", () => console.log('port is open', port.baudRate))
 port.on('data', (data) => console.log(data))
@@ -29,45 +35,55 @@ port.on('error', (err) => {
 
 let w = 24;
 
-const res = new Buffer.alloc(4);
+const res = new Buffer(4);
 res[0] = w;
 res[1] = w >> 8
 w = Math.round(91.4285714);
 res[2] = w;
 res[3] = w >> 8;
 
+const resByte = new Uint8Array(res);
+
 let connector;
 
+const getIdBuffer = new Buffer(5);
+getIdBuffer[0] = 5;
+getIdBuffer[1] = 4;
+getIdBuffer[2] = 1;
+getIdBuffer[3] = 0x14;
+getIdBuffer[4] = 0;
 
-const getIdBuffer = new Buffer.alloc(5, [5, 4, 1, 0x14, 0])
+const idByteArray = new Uint8Array(getIdBuffer)
 
-port.write(getByteFrame(getIdBuffer, 11), async(err, res) => {
-    console.log(getIdBuffer)
-    console.log(getByteFrame(getIdBuffer, 11))
-    console.log('err', await err)
-    console.log('res', await res)
+port.write(getByteFrame(getIdBuffer, 11), function(err, res) {
+    if (err) {
+        console.log('write1 err =', err);
+    } else {
+        console.log('write1 res =', res);
+        port.write(getByteFrame(buffer, 11), function(err, bytesWritten) {
+            if (err) {
+                console.log('write2 err =', err)
+            } else {
+                console.log('write2 res =', bytesWritten);
+                port.write(getByteFrame(buffer, 11), function(err, bytesWritten) {
+                    if (err) {
+                        console.log('write3 err =', err)
+                    } else {
+                        console.log('write3 res =', bytesWritten);
+                    }
+                })
+            }
+        })
+    }
 })
 
-port.write(getByteFrame(buffer, 11), async(err, bytesWritten) => {
-    console.log(buffer)
-    console.log("calbackErr", await err);
-    console.log("bytesWritten", await bytesWritten);
-})
-
-port.write(getByteFrame(buffer, 11), async(err, bytesWritten) => {
-    console.log(buffer)
-    console.log("calbackErr", await err);
-    console.log("bytesWritten", await bytesWritten);
-})
-
-port.write(GetReqToSendData(connector, 0x25, 1, res, 4), async(err, res) => {
-    console.log('lol', GetReqToSendData(connector, 0x25, 1, res, 4))
-    console.log(await err);
-    console.log(await res);
-})
+// port.write(GetReqToSendData(), async(err, res) => {
+//     console.log(await err)
+//     console.log(await res)
+// })
 
 function GetReqToSendData(conn, tag, param, buf, len) {
-    const dfBuf = new Buffer.alloc(11);
+    const dfBuf = new ArrayBuffer(11);
     dfBuf[0] = 1 + 1 + 1 + 2 + 1 + 1 + len;
     dfBuf[1] = 4;
     dfBuf[2] = 0;
@@ -76,34 +92,63 @@ function GetReqToSendData(conn, tag, param, buf, len) {
     dfBuf[5] = tag;
     dfBuf[6] = param;
 
+    const dfByteBuff = new Uint8Array(dfBuf)
+
     if (buf != null)
+    {
+        for (let i = 0; i < len; i++)
         {
-            for (let i = 0; i < len; i++)
-            {
-                dfBuf[i + 7] = buf[i];
-            }
+            dfByteBuff[i + 7] = buf[i];
         }
+    }
 
-
-    return getByteFrame(dfBuf, dfBuf[0]);
+    return getByteFrame(dfByteBuff, dfByteBuff[0]);
 }
 
 function getByteFrame(buff, len){
-    const pba = new Buffer.alloc(16);
+    const pba = new ArrayBuffer(16);
+
+    const bytePba = new Uint8Array(pba)
 
     for (i = 0; i < len; i++) {
-        pba[i] = buff[i]
+        bytePba[i] = buff[i]
     }
 
-    pba[0] = pba[0] + 2;
+    bytePba[0] = bytePba[0] + 2;
 
-    const crcDec = crc.crc16(pba).toString(len);
+    const crcDec = crc16(bytePba, len);
 
-    pba[len] = crcDec & 0xFF;
-    pba[len + 1] = crcDec >> 8 & 0xFF;
+    bytePba[len] = crcDec & 0xFF;
+    bytePba[len + 1] = crcDec >> 8 & 0xFF;
 
-    return pba;
+    return bytePba;
 }
+
+function crc16(buf, BufferLength)
+        {
+            const CRC_CONST = 0xA001;
+            let pba = buf;
+            let crc = 0xFFFF;
+
+            for (let i = 0; i < BufferLength; i++)
+            {
+                crc = crc ^ pba[i];
+                for (let j = 0; j <= 7; j++)
+                {
+                    if ((crc & 1) != 0)
+                    {
+                        crc = crc >> 1;
+                        crc = crc ^ CRC_CONST;
+                    }
+                    else
+                    {
+                        crc = crc >> 1;
+                    }
+                }
+            }
+           
+            return crc;
+        }
 
 port.on('data', (data) => console.log(data))
 port.on('close', () => console.log('closed')) 
